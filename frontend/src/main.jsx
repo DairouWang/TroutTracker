44a874d7b7c3670abae583fe236611d243765104
Dairou Wang <dairou2021@gmail.com>
2025-11-26 20:02:09 -0800

    Connect to Mixpanel, [skip scraper]

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import mixpanel from 'mixpanel-browser'
 import App from './App.jsx'
 import './index.css'
+
+// Initialize Mixpanel Autocapture
+mixpanel.init('3cdea1dcd4fe1af85df5b75926c84954', {
+  autocapture: true,
+  record_sessions_percent: 100,
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,5 +18,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>,
 )
-
 
